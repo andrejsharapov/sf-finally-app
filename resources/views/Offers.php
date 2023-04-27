39ee29f534f46418112870fad9178936b1bc538f
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2023-04-27 17:21:11 +0300

    dev: add follow state

--- resources/views/Offers.php
+++ resources/views/Offers.php
@@ -116,7 +116,28 @@ if (!isset($user_id)) {
                         <div class="card-body">
                             <h4 class="card-title d-flex justify-content-between" title="id автора предложения">
                                 <?= $val['title']; ?>
-                                <span class="opacity-25"><?= $val['user_id']; ?></span>
+
+                                <?php if (isset($user) && $user['role_id'] == '3'): ?>
+                                    <form method="post">
+                                        <input type="hidden" name="form" value="following">
+                                        <input type="hidden" name="send_id" value="<?php echo $val['id']; ?>"
+                                        <input type="hidden" name="send_id" value="<?php echo $user['id']; ?>"
+                                        <button type="submit" class="btn btn-primary">
+                                            <svg width="24" height="24" fill="orange" xmlns="http://www.w3.org/2000/svg"
+                                                 viewBox="0 0 24 24">
+                                                <title>Unfollow</title>
+                                                <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"/>
+                                            </svg>
+                                            <span class="opacity-50">
+                                                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"
+                                                     viewBox="0 0 24 24">
+                                                    <title>Follow</title>
+                                                    <path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z"/>
+                                                </svg>
+                                            </span>
+                                        </button>
+                                    </form>
+                                <?php endif; ?>
                             </h4>
 
                             <h6 class="card-subtitle mb-2 text-body-secondary">
@@ -156,7 +177,7 @@ if (!isset($user_id)) {
                                 </div>
                                 <?php if (isset($user) && $user['role_id'] == '3'): ?>
                                     <form method="post" class="form_send">
-                                        <input type="hidden" name="form" value="" class="">
+                                        <input type="hidden" name="form" value="">
                                         <input type="hidden" name="send_id" value="<?php echo $val['id']; ?>"
                                                class="form_send-id">
                                         <input type="hidden" name="send_payment" value="<?php echo $val['payment']; ?>"
