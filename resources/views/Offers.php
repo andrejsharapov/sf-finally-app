cf3e606e2f2a60dfb10f47a384f8cdfc611b81b8
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2023-04-28 13:11:05 +0300

    add `un/following` actions

--- resources/views/Offers.php
+++ resources/views/Offers.php
@@ -124,18 +124,22 @@ if (!isset($user_id)) {
                                         <input type="hidden" name="author_id" value="<?php echo $val['creator_id']; ?>">
                                         <input type="hidden" name="follower_id" value="<?php echo $user['id']; ?>">
                                         <button type="submit" class="btn btn-light">
-                                            <svg width="24" height="24" fill="orange" xmlns="http://www.w3.org/2000/svg"
-                                                 viewBox="0 0 24 24">
-                                                <title>Unfollow</title>
-                                                <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"/>
-                                            </svg>
-<!--                                            <span class="opacity-50">-->
-<!--                                                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"-->
-<!--                                                     viewBox="0 0 24 24">-->
-<!--                                                    <title>Follow</title>-->
-<!--                                                    <path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z"/>-->
-<!--                                                </svg>-->
-<!--                                            </span>-->
+                                            <?php if ($val['following'] == 1): ?>
+                                                <svg width="24" height="24" fill="orange"
+                                                     xmlns="http://www.w3.org/2000/svg"
+                                                     viewBox="0 0 24 24">
+                                                    <title>Unfollow</title>
+                                                    <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"/>
+                                                </svg>
+                                            <?php else: ?>
+                                                <span class="opacity-50">
+                                                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"
+                                                     viewBox="0 0 24 24">
+                                                    <title>Follow</title>
+                                                    <path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z"/>
+                                                </svg>
+                                            </span>
+                                            <?php endif; ?>
                                         </button>
                                     </form>
                                 <?php endif; ?>
@@ -167,7 +171,7 @@ if (!isset($user_id)) {
                                             <?= $val['transitions']; ?>
                                         </div>
                                         <div class="d-flex justify-content-between mb-1">
-                                            <span>→ Общий доход</span>
+                                            <span>→ Расходы</span>
                                             <?= $val['total_cost'] . ' руб.'; ?>
                                         </div>
                                         <div class="text-nowrap mb-1 overflow-hidden w-100 text-truncate">
@@ -227,13 +231,3 @@ if (!isset($user_id)) {
         <?php endforeach; ?>
     <?php endif; ?>
 </div>
-
-<?php
-if (!empty($_SESSION['checkFollow'])) {
-    echo "<div class='snackbar shadow conainer-sm mx-auto mt-5 rounded p-3 border bg-$_SESSION[errors]-subtle font-weight-medium'>";
-    echo $_SESSION['checkFollow'];
-    echo '</div>';
-
-    unset($_SESSION['checkFollow']);
-}
-?>
