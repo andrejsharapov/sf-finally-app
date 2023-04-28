6f99554413a03dac8acb916491e1945f52f10ff7
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2023-04-28 10:20:51 +0300

    add `follow` action

--- resources/views/Offers.php
+++ resources/views/Offers.php
@@ -121,7 +121,7 @@ if (!isset($user_id)) {
                                     <form method="post">
                                         <input type="hidden" name="form" value="following">
                                         <input type="hidden" name="offer_id" value="<?php echo $val['id']; ?>">
-                                        <input type="hidden" name="author_id" value="<?php echo $val['user_id']; ?>">
+                                        <input type="hidden" name="author_id" value="<?php echo $val['creator_id']; ?>">
                                         <input type="hidden" name="follower_id" value="<?php echo $user['id']; ?>">
                                         <button type="submit" class="btn btn-light">
                                             <svg width="24" height="24" fill="orange" xmlns="http://www.w3.org/2000/svg"
@@ -129,13 +129,13 @@ if (!isset($user_id)) {
                                                 <title>Unfollow</title>
                                                 <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"/>
                                             </svg>
-                                            <span class="opacity-50">
-                                                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"
-                                                     viewBox="0 0 24 24">
-                                                    <title>Follow</title>
-                                                    <path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z"/>
-                                                </svg>
-                                            </span>
+<!--                                            <span class="opacity-50">-->
+<!--                                                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"-->
+<!--                                                     viewBox="0 0 24 24">-->
+<!--                                                    <title>Follow</title>-->
+<!--                                                    <path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z"/>-->
+<!--                                                </svg>-->
+<!--                                            </span>-->
                                         </button>
                                     </form>
                                 <?php endif; ?>
