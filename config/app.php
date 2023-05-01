c1000525970e242518e874d985271afa0c4db21c
Andrej Sharapov <an.sharapov90@gmail.com>
2023-05-01 13:23:51 +0300

    fix paths

--- config/app.php
+++ config/app.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once dirname(__DIR__) . '\app\Models\Model.php';
-require_once dirname(__DIR__) . '\resources\views\View.php';
-require_once dirname(__DIR__) . '\app\Http\Controllers\Controller.php';
-require_once dirname(__DIR__) . '\routes\Route.php';
-require_once dirname(__DIR__) . '\database\db.php';
+require_once dirname(__DIR__) . '/app/Models/Model.php';
+require_once dirname(__DIR__) . '/resources/views/View.php';
+require_once dirname(__DIR__) . '/app/Http/Controllers/Controller.php';
+require_once dirname(__DIR__) . '/routes/Route.php';
+require_once dirname(__DIR__) . '/database/db.php';
 
 (new Route)->start();
 
